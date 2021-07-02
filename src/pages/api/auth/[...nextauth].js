e6e2dfc70a5b420aae5aaeb8e68d39709366be67
Elias de Oliveira <eliasdeoliveira@iMac-de-Elias.local>
2021-07-02 20:08:57 -0300

    Elias - 02/07/2021 - 3

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,7 +3,11 @@ import Providers from 'next-auth/providers'
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
-        Providers.Google(global.authGoogle),
+        Providers.Google({
+            clientId: '612142441301-j3g2itt83ld73cc09brro1nfhsa7onbc.apps.googleusercontent.com',
+            clientSecret: 'ZoQ6UgeswMltSol_r4gSLCb5',
+            authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+        }),
         Providers.Facebook({
             clientId: '506957027085535',
             clientSecret: '7d7c45d959678c74558d53ed40a4493f'
