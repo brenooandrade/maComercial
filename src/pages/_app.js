911522136d4b4212f3ac3bd663d32b7d98f9abe0
Elias de Oliveira <eliasdeoliveira@iMac-de-Elias.local>
2021-07-03 14:50:42 -0300

    Elias - 03/07/2021 - 2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,15 @@
 // import '../styles/globals.css'
 global.ambiente = 'production';
-if (global.ambiente == 'production') {
+if (global.ambiente == 'development') {
   global.authGoogle = {
     clientId: '612142441301-j3g2itt83ld73cc09brro1nfhsa7onbc.apps.googleusercontent.com',
     clientSecret: 'ZoQ6UgeswMltSol_r4gSLCb5',
     authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
   };
-  global.tokenMP = 'TEST-8952990287765540-061620-969fb3326495eed18bdc5d06f04ecef8-775914546';
+  global.tokenMP = 'APP_USR-8952990287765540-061620-181068cadf106e0eafa1d31f0beaa031-775914546';
   global.linkRetornoMP = 'https://assinaturameuarquivo.com.br/selecione-um-plano/2';
-  global.linkDashboard = 'https://searchcloud.com.br/login/primeiro-acesso';
-  global.api = 'https://api.searchcloud.com.br';
+  global.linkDashboard = 'https://app.meuarquivo.com.br/login/primeiro-acesso';
+  global.api = 'https://apisearchcloud.com.br';
 } else if (global.ambiente == 'development') {
   global.authGoogle = {
     clientId: '612142441301-j3g2itt83ld73cc09brro1nfhsa7onbc.apps.googleusercontent.com',
@@ -17,10 +17,22 @@ if (global.ambiente == 'production') {
     authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
   };
   global.tokenMP = 'TEST-8952990287765540-061620-969fb3326495eed18bdc5d06f04ecef8-775914546';
-  global.linkRetornoMP = 'http://localhost:3000/selecione-um-plano/2';
-  global.linkDashboard = 'http://localhost/app.meuarquivo/login/primeiro-acesso';
-  global.api = 'http://localhost:5000';
+  global.linkRetornoMP = 'https://link-pagamento-nextjs.vercel.app';
+  global.linkDashboard = 'https://searchcloud.com.br/login/primeiro-acesso';
+  global.api = 'http://api.searchcloud.com.br';
 }
+
+// else if (global.ambiente == 'development') {
+//   global.authGoogle = {
+//     clientId: '612142441301-j3g2itt83ld73cc09brro1nfhsa7onbc.apps.googleusercontent.com',
+//     clientSecret: 'ZoQ6UgeswMltSol_r4gSLCb5',
+//     authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+//   };
+//   global.tokenMP = 'TEST-8952990287765540-061620-969fb3326495eed18bdc5d06f04ecef8-775914546';
+//   global.linkRetornoMP = 'http://localhost:3000/selecione-um-plano/2';
+//   global.linkDashboard = 'http://localhost/app.meuarquivo/login/primeiro-acesso';
+//   global.api = 'http://localhost:5000';
+// }
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
