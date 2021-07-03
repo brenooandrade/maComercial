2fd252eb726a9fd52baeda24f463246889c8bf5e
Elias de Oliveira <eliasdeoliveira@iMac-de-Elias.local>
2021-07-03 15:56:10 -0300

    Elias - 03/07/2021 - 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://api.searchcloud.com.br',
+    baseURL: process.env.DEVELOPMENT_API,
     // baseURL: global.api,
     headers: {
         'Content-Type': 'application/json',
