b3e432bdddba9fc59ebf9b46278b2c6fd0c6de30
Elias de Oliveira <eliasdeoliveira@iMac-de-Elias.local>
2021-07-04 16:34:59 -0300

    Elias - 04/07/2021 - 4

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: urlAPi,
+    baseURL: process.env.DEVELOPMENT_API,
     // baseURL: global.api,
     headers: {
         'Content-Type': 'application/json',
