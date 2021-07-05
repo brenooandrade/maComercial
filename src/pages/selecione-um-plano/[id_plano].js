ee913250034888bdff6a0fb0d1b05c7606a0b696
Elias de Oliveira <eliasdeoliveira@iMac-de-Elias.local>
2021-07-05 17:02:57 -0300

    Elias - 05/07/2021 - 10

--- src/pages/selecione-um-plano/[id_plano].js
+++ src/pages/selecione-um-plano/[id_plano].js
@@ -986,7 +986,7 @@ export default function Home({ urlAPi, tokenMP, linkRetornoMP, linkDashboard }) 
                 onSuccess={responseGoogle}
                 onFailure={responseGoogle}
                 cookiePolicy={'single_host_origin'} 
-                className="bg-dark"
+                className="rounded-pill shadow-none"
                 style={{
                   borderRadius: 10
                 }}
